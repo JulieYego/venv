01622e2787f0716b448085da5ae27d895d7d132d
Julie Yego <julieyego@strathmore.edu>
2022-11-13 01:08:31 +0300

    message_flashing

--- flask_j/website/auth.py
+++ flask_j/website/auth.py
@@ -1,18 +1,35 @@
 # store routes for authentication
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('login')
+@auth.route('login', methods=['GET', 'POST'])
 def login():
-    # return render_template('login.html')
     return render_template('login.html', ship='Wilmon')
 
 
-@auth.route("/register")
+@auth.route("/register", methods=['GET', 'POST'])
 def register():
-    # return render_template('register.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        # check if information is valid
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(username) < 2:
+            flash('Username must be greater than 1 character', category='error')
+        elif password != confirm_password:
+            flash('Passwords don\'t match', category='error')
+        elif len(password) < 7:
+            flash('Password must be atleast 7 characters', category='error')
+        else:
+            # add user to db
+            flash('Account created!', category='success')
+
     return render_template('register.html')
 
 
