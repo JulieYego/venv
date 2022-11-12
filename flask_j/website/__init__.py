5406aedb7a47ee312eeaeef1581b85ce2c352461
Julie Yego <julieyego@strathmore.edu>
2022-11-13 02:33:03 +0300

    login_module

--- flask_j/website/__init__.py
+++ flask_j/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -22,10 +23,24 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     # from .models import User, Note  # import models file
+    #from .models import User, Note
     from . import models
     # create_database(app)
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return models.User.query.get(int(id))
+
+    '''
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))'''
 
     return app
 
